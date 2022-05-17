a2c7e2e54a7dea8d300108e3eb93bc339696ec0a
Zishan-7 <zishan2539@gmail.com>
2022-05-17 15:15:32 +0530

    Opensea route added

--- Contract/constants/index.js
+++ Contract/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x96f6Acb19Cf29Ab6faDc5AF5b4824111CDE43F24";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-eta-teal.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
